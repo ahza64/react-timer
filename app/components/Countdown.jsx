1f6f20958db0d8f83def6429308b3b38d4678716
ahza64 <ahza64@yahoo.com>
2016-11-06 01:10:43 -0700

    timer counter

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -5,11 +5,36 @@ var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
   getInitialState: function () {
-    return {count: 0};
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    // check if component change is in the 'countdownStatus'
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      // check to see if "countdownStatus: 'started'"
+      switch (this.state.countdownStatus) {
+        // when status is 'started', call 'startTimer'
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval (() => {
+      var newCount = this.state.count -1;
+      this.setState({
+        // stop count at 0
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000);
   },
   handleSetCountdown: function (seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started'
     });
   },
   render: function () {
