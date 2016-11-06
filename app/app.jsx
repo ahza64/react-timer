a292413797befc9b7248b9a0eaef87e34eb35c04
ahza64 <ahza64@yahoo.com>
2016-11-05 18:26:29 -0700

    FE routing

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,8 @@ var ReactDOM = require('react-dom');
 // object destructuring syntax
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 // load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
@@ -14,7 +16,8 @@ require('style!css!sass!applicationStyles')
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-      
+      <Route path="countdown" component={Countdown}/>
+      <IndexRoute component={Timer}/>
     </Route>
   </Router>,
   document.getElementById('app')
